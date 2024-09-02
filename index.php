764a56728a53b1b050096e834033aefdccc240b6
PaintX0 <oscar.poan@gmail.com>
2024-09-02 14:27:20 +0200

    tabel mal

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
-test
+<html>
+    <head>
+    </head>
+    <body>
+        <?php 
+        $BussTider = Buss + Tid;
+
+             if (2 > 1) {
+                echo "<h1>Buss Tider</h1>";
+             }
+            for ($i = 0; $i <= 10; $i++) {
+                echo  "<div>Buss</div>";
+                echo " <div style='border: 1px solid;padding-bottom:5px;'>Rad: ". $i ." </div>";
+            }
+            
+        ?>
+
+    </body>
+    </html>
