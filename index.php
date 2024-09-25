210e941f5799aa21b807fb8e089f6c1df4e0e63a
PaintX0 <oscar.poan@gmail.com>
2024-09-25 22:00:20 +0200

    css

--- index.php
+++ index.php
@@ -9,53 +9,58 @@
 
 </head>
 <body>
+  
+  <div class="view">
 
-  <div class="flex-container">
-    <img class="banner">
-    <div class="column-container">
-
-      <div class="column">
-      <?php
-          for ($i = 1; $i <= 11; $i++) {
-            ?>
-          <div class="tabelRow">
-            <div class="transport">
-             <p class="number"> Buss </p> 
-             <img src="gifs/bus.gif" width="50px" class="gifar">
-            </div>
-            <div class="time">
-              <div> <div>XX:XX</div> </div>
-              <div> <div>15 Min</div> </div>
-            </div>
-          </div>
-          <?php
-          }
-      ?>
-      
-      </div>
-
-      <div class="column">
-      <?php
-          for ($i = 1; $i <= 11; $i++) {
-            ?>
-          <div class="tabelRow">
-            <div class="transport">
-             <p class="number"> Train </p> 
-             <img src="gifs/train.gif" width="50px" class="gifar">
-            </div>
-            <div class="time">
-              <div><div>XX:XX</div></div>
-              <div><div>15 Min</div></div>
-            </div>
-          </div>
-          <?php
-          }
-      ?>
-      
-      </div>
-
-    </div>
+    <img class="banner" src="Bilder/Asset 1Iris_symbol.svg">
     
+    <div class="flex-container">
+  
+      <div class="column-container">
+  
+        <div class="column">
+        <?php
+            for ($i = 1; $i <= 11; $i++) {
+              ?>
+            <div class="tabelRow">
+              <div class="transport">
+               <p class="number"> Buss </p> 
+               <img src="gifs/bus.gif" width="50px" class="gifar">
+              </div>
+              <div class="time">
+                <div> <div>XX:XX</div> </div>
+                <div> <div>15 Min</div> </div>
+              </div>
+            </div>
+            <?php
+            }
+        ?>
+        
+        </div>
+  
+        <div class="column">
+        <?php
+            for ($i = 1; $i <= 11; $i++) {
+              ?>
+            <div class="tabelRow">
+              <div class="transport">
+               <p class="number"> Train </p> 
+               <img src="gifs/train.gif" width="50px" class="gifar">
+              </div>
+              <div class="time">
+                <div><div>XX:XX</div></div>
+                <div><div>15 Min</div></div>
+              </div>
+            </div>
+            <?php
+            }
+        ?>
+        
+        </div>
+  
+      </div>
+      
+    </div>
   </div>
 
 </body>
